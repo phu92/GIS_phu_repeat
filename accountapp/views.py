4493b2ecec9e13b400793ca2cc2861a38d0731c7
Park Hyun-uk <teroto@naver.com>
2021-07-09 12:58:57 +0900

    7.9 phu_repeat 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,8 +3,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from accountapp.models import NewModel
+
+
 def hello_world(request):
-    if request.method =="POST":
-        return render(request, 'accountapp/hello_world.html',context={'text':'POST METHOD!'})
+    if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_model = NewModel()
+        new_model.text = temp
+        new_model.save()
+
+        return render(request, 'accountapp/hello_world.html', context={'new_model':new_model})
     else:
-        return render(request, 'accountapp/hello_world.html',context={'text':'GET METHOD!'})
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
