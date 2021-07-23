2274f1332de608df80343a47abbf9a82c14539fc
Park Hyun-uk <teroto@naver.com>
2021-07-23 11:32:08 +0900

    7.23 git commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,6 +12,8 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
+from accountapp.templates.accountapp.decorators import account_ownership_required
+
 
 @login_required#(login_url=reverse_lazy('accountapp:login')) 다른 경로일 경우
 def hello_world(request):
@@ -41,8 +43,10 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+has_ownership = [login_required, account_ownership_required]
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView): #crete view 참조
     model = User
     form_class = AccountCreationForm
@@ -51,8 +55,8 @@ class AccountUpdateView(UpdateView): #crete view 참조
     #success_url = reverse_lazy('accountapp:detail') #아직 사용 불가
     template_name = 'accountapp/update.html'
 
-@method_decorator(login_required, 'get')
-@method_decorator(login_required, 'post')
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'#유저 정보에 접근하기 위해
