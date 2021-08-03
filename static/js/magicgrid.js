ecc5b40b654f4568d25ba81d23d8730121149755
Park Hyun-uk <teroto@naver.com>
2021-08-03 11:40:05 +0900

    8.3 git commit 2

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -276,10 +276,18 @@ MagicGrid.prototype.listen = function listen () {
 
 let magicGrid = new MagicGrid({
   container: '.container',
-  animate: true,
+  animate: false,
   gutter: 30,
   static: true,
   useMin: true
 });
 
 magicGrid.listen();
+
+var masonrys = document.getElementsByTagName( "img")
+
+for (let i=0; masonrys.length; i ++) {
+  masonrys[i].addEventListener('load', function () {
+    magicGrid.positionItems();
+  },false)
+}
