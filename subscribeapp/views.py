1ec01b42ccaba86afe5615f816f9384f79acecc0
Park Hyun-uk <teroto@naver.com>
2021-08-20 18:12:59 +0900

    8.20 git commit 4

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, ListView
 
+from articleapp.models import Article
 from projectapp.models import Project
 from subscribeapp.models import Subscription
 
@@ -25,3 +26,14 @@ class SubscriptionView(RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('projectapp:detail', kwargs={'pk': kwargs['project_pk']})
+
+class SubscriptionListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'subscribeapp/list.html'
+    paginate_by = 20
+
+    def get_queryset(self):#지정한 내용만 설정
+        project_list = Subscription.objects.filter(user=self.request.user).values_list('project')
+        article_list = Article.objects.filter(project__in=project_list)#조건에 만족하는게 있다면 가져온다.
+        return article_list
